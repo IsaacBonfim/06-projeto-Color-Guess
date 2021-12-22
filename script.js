898f62ce195a39af595ddf4a80e404ed310aec9a
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2021-12-22 16:00:49 -0300

    Enviando requisito 5

--- script.js
+++ script.js
@@ -19,3 +19,28 @@ function alocaCores() {
 }
 
 window.onload = alocaCores();
+
+const resposta = document.getElementById('answer');
+
+function selecao(evento) {
+  const selecionado = evento.target;
+
+  for (let i = 0; i < cores.length; i += 1) {
+    cores[i].style.border = '1px solid rgba(0, 0, 0, 1)';
+    cores[i].style.boxShadow = '2px 2px 3px rgba(0, 0, 0, 1)';
+    selecionado.style.border = '3px solid rgba(61, 220, 151, 1)';
+    selecionado.style.boxShadow = '2px 2px 3px rgba(37, 110, 255, 1)';
+  }
+
+  const cor = selecionado.style.backgroundColor;
+
+  if (codigo.textContent === cor) {
+    resposta.textContent = 'Acertou!';
+  } else {
+    resposta.textContent = 'Errou! Tente novamente!';
+  }
+}
+
+for (let i = 0; i < cores.length; i += 1) {
+  cores[i].addEventListener('click', selecao);
+}
